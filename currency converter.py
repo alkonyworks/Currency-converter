9826263399f9609666e0c2fb8488db30300ccfc4
alkonyworks <50152711@stu.gbmc.ac.uk>
2026-09-07 12:55:21 +0100

    added more

--- currency converter.py
+++ currency converter.py
@@ -2,4 +2,12 @@ from tkinter import *
 
 window = Tk()
 window.geometry("700x500")
-window.mainloop()
+
+LBL_pounds = Label(window, text="Pounds")
+LBL_pounds.pack()
+
+txt_pounds = Entry(window, width = 15)
+txt_pounds.pack()
+
+#btn_convert = Buttom
+#window.mainloop()
